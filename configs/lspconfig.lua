44d486a05832dfca0932050e4a840a69744650f4
Zayaan-taha <zayaan.taha@icloud.com>
2024-08-02 10:02:58 +0530

    Basic python config
    Added tiny-inline-diagnostic.nvim for minimalistic diagnositcs

---
+++ configs/lspconfig.lua
@@ -0,0 +1,32 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+-- Needs to be configured explicity due to root_dir
+lspconfig.pyright.setup {
+  root_dir = vim.loop.cwd,
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+
+}
+
+-- Enable tiny inline diagnostics on in python files
+if vim.bo.filetype == 'python' then
+  vim.diagnostic.config({ virtual_text = false })
+end
+
---
+++ plugins/init.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', 
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+      },
+    },
+  },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("tiny-inline-diagnostic").setup()
+    end,
+  },
+}
