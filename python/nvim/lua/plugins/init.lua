1694f789680e77fe32548d110d00d392f8564495
Zayaan-taha <zayaan.taha@icloud.com>
2025-08-18 17:06:44 +0530

    Added formatting support for python

--- python/nvim/lua/plugins/init.lua
+++ python/nvim/lua/plugins/init.lua
@@ -78,15 +78,6 @@ return {
     end,
   },
   {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
-  {
     "mfussenegger/nvim-dap",
   },
   -- rust
