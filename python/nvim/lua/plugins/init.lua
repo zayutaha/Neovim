9ea905bb67d7c4014a2e451399b1a852c254965a
Zayaan-taha <zayaan.taha@icloud.com>
2025-08-18 18:22:54 +0530

    Added avante.nvim

--- python/nvim/lua/plugins/init.lua
+++ python/nvim/lua/plugins/init.lua
@@ -875,6 +875,15 @@ return {
     },
   },
   {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+  },
+  {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
       { "github/copilot.vim" },
