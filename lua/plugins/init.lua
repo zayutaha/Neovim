ce1bf3db8eaba51f4ebd9b260089ad330935c811
zayutaha <zayaan.taha@icloud.com>
2025-12-20 22:27:57 +0300

    Moved rust plugins to rust.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 local cmp = require "cmp"
 
 return {
+  {import  = "plugins.rust"},
   {
     "stevearc/conform.nvim",
     event = "BufWritePre",
@@ -61,35 +62,6 @@ return {
       }
       table.insert(M.sources, { name = "crates" })
       return M
-    end,
-  },
-  -- rust
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^4",
-    ft = { "rust" },
-    dependencies = "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.languages.rust.opts"
-    end,
-  },
-  {
-    "saecki/crates.nvim",
-    ft = { "toml" },
-    config = function(_, opts)
-      local crates = require "crates"
-      crates.setup(opts)
-      require("cmp").setup.buffer {
-        sources = { { name = "crates" } },
-      }
-      crates.show()
-    end,
-  },
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function()
-      vim.g.rustfmt_autosave = 1
     end,
   },
   -- go
---
+++ lua/plugins/rust.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require "configs.languages.rust.opts"
+    end,
+  },
+
+  {
+    "saecki/crates.nvim",
+    ft = { "toml" },
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    opts = {},
+  },
+}
+
