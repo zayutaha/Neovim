ba5e1293dd161f8fa70b81d0fc699c3d1a96abbb
zayutaha <zayaan.taha@icloud.com>
2025-12-21 19:05:39 +0300

    Shifted images and diagram.nvim to media.lua
    - Commented it out bcz I"m not using it

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,7 @@ local cmp = require "cmp"
 return {
   { import = "configs.languages.rust.plugins" },
   { import = "configs.core.plugins" },
+  -- { import = "configs.core.media" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -119,63 +120,6 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
   },
-  -- {
-  --   "3rd/image.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("image").setup {
-  --       backend = "kitty",
-  --     }
-  --   end,
-  -- },
-  -- {
-  --   "3rd/diagram.nvim",
-  --   dependencies = {
-  --     "3rd/image.nvim",
-  --   },
-  --   ft = { "markdown" },
-  --   config = function()
-  --     require("diagram").setup {
-  --       integrations = {
-  --         require "diagram.integrations.markdown",
-  --         require "diagram.integrations.neorg",
-  --       },
-  --       renderer_options = {
-  --         mermaid = {
-  --           theme = "forest",
-  --         },
-  --         plantuml = {
-  --           charset = "utf-8",
-  --         },
-  --         d2 = {
-  --           theme_id = 1,
-  --         },
-  --         gnuplot = {
-  --           theme = "dark",
-  --           size = "800,600",
-  --         },
-  --       },
-  --     }
-  --   end,
-  --   opts = { -- you can just pass {}, defaults below
-  --     command_paths = {
-  --       mermaid = "/opt/homebrew/bin/mmdc", -- Adjust this path based on your installation
-  --     },
-  --     events = {
-  --       render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
-  --       clear_buffer = { "BufLeave" },
-  --     },
-  --     renderer_options = {
-  --       mermaid = {
-  --         background = "transparent",
-  --         theme = "forest",
-  --         width = 800,
-  --         height = 400,
-  --         scale = 1,
-  --       },
-  --     },
-  --   },
-  -- },
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
