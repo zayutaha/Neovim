d7f4db67db05c2975e75da3881fc53b0b0c4da6c
zayutaha <zayaan.taha@icloud.com>
2025-12-21 19:17:08 +0300

    Shifted python import to tools

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,7 @@ local cmp = require "cmp"
 return {
   { import = "configs.languages.rust.plugins" },
   { import = "configs.core.plugins" },
+  { import = "configs.core.ui" },
   -- { import = "configs.core.media" },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -60,55 +61,6 @@ return {
         end,
       })
     end,
-  },
-  {
-    "kiyoon/python-import.nvim",
-    build = "uv tool install . --force --reinstall",
-    opts = {
-      -- Example 1:
-      -- Default behaviour for `tqdm` is `from tqdm.auto import tqdm`.
-      -- If you want to change it to `import tqdm`, you can set `import = {"tqdm"}` and `import_from = {tqdm = vim.NIL}` here.
-      -- If you want to change it to `from tqdm import tqdm`, you can set `import_from = {tqdm = "tqdm"}` here.
-
-      -- Example 2:
-      -- Default behaviour for `logger` is `import logging`, ``, `logger = logging.getLogger(__name__)`.
-      -- If you want to change it to `import my_custom_logger`, ``, `logger = my_custom_logger.get_logger()`,
-      -- you can set `statement_after_imports = {logger = {"import my_custom_logger", "", "logger = my_custom_logger.get_logger()"}}` here.
-      extend_lookup_table = {
-        ---@type string[]
-        import = {
-          -- "tqdm",
-        },
-
-        ---@type table<string, string|vim.NIL>
-        import_as = {
-          -- These are the default values. Here for demonstration.
-          np = "numpy",
-          pd = "pandas",
-        },
-
-        ---@type table<string, string|vim.NIL>
-        import_from = {
-          -- tqdm = vim.NIL,
-          -- tqdm = "tqdm",
-        },
-
-        ---@type table<string, string[]|vim.NIL>
-        statement_after_imports = {
-          -- logger = { "import my_custom_logger", "", "logger = my_custom_logger.get_logger()" },
-        },
-      },
-
-      ---Return nil to indicate no match is found and continue with the default lookup
-      ---Return a table to stop the lookup and use the returned table as the result
-      ---Return an empty table to stop the lookup. This is useful when you want to add to wherever you need to.
-      ---@type fun(winnr: integer, word: string, ts_node: TSNode?): string[]?
-      custom_function = function(winnr, word, ts_node)
-        -- if vim.endswith(word, "_DIR") then
-        --   return { "from my_module import " .. word }
-        -- end
-      end,
-    },
   },
 
   {
