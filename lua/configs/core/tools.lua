6fd4d7983297251f750344dcffee8ca0a5b25898
zayutaha <zayaan.taha@icloud.com>
2025-12-21 20:15:33 +0300

    cleaned up python import

--- lua/configs/core/tools.lua
+++ lua/configs/core/tools.lua
@@ -2,51 +2,8 @@ return {
   {
     "kiyoon/python-import.nvim",
     build = "uv tool install . --force --reinstall",
-    opts = {
-      -- Example 1:
-      -- Default behaviour for `tqdm` is `from tqdm.auto import tqdm`.
-      -- If you want to change it to `import tqdm`, you can set `import = {"tqdm"}` and `import_from = {tqdm = vim.NIL}` here.
-      -- If you want to change it to `from tqdm import tqdm`, you can set `import_from = {tqdm = "tqdm"}` here.
-
-      -- Example 2:
-      -- Default behaviour for `logger` is `import logging`, ``, `logger = logging.getLogger(__name__)`.
-      -- If you want to change it to `import my_custom_logger`, ``, `logger = my_custom_logger.get_logger()`,
-      -- you can set `statement_after_imports = {logger = {"import my_custom_logger", "", "logger = my_custom_logger.get_logger()"}}` here.
-      extend_lookup_table = {
-        ---@type string[]
-        import = {
-          -- "tqdm",
-        },
-
-        ---@type table<string, string|vim.NIL>
-        import_as = {
-          -- These are the default values. Here for demonstration.
-          np = "numpy",
-          pd = "pandas",
-        },
-
-        ---@type table<string, string|vim.NIL>
-        import_from = {
-          -- tqdm = vim.NIL,
-          -- tqdm = "tqdm",
-        },
-
-        ---@type table<string, string[]|vim.NIL>
-        statement_after_imports = {
-          -- logger = { "import my_custom_logger", "", "logger = my_custom_logger.get_logger()" },
-        },
-      },
-
-      ---Return nil to indicate no match is found and continue with the default lookup
-      ---Return a table to stop the lookup and use the returned table as the result
-      ---Return an empty table to stop the lookup. This is useful when you want to add to wherever you need to.
-      ---@type fun(winnr: integer, word: string, ts_node: TSNode?): string[]?
-      custom_function = function(winnr, word, ts_node)
-        -- if vim.endswith(word, "_DIR") then
-        --   return { "from my_module import " .. word }
-        -- end
-      end,
-    },
+    ft = "python",
+    opts = require "configs.languages.python.opts",
   },
   {
     "ruifm/gitlinker.nvim",
@@ -56,5 +13,10 @@ return {
     end,
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
+  },
+  {
+    "esmuellert/vscode-diff.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    cmd = "CodeDiff",
   },
 }
---
+++ lua/configs/languages/python/opts.lua
@@ -0,0 +1,12 @@
+return {
+  extend_lookup_table = {
+    import = {},
+    import_as = {
+      np = "numpy",
+      pd = "pandas",
+    },
+    import_from = {},
+    statement_after_imports = {},
+  },
+  custom_function = function(winnr, word, ts_node) end,
+}
